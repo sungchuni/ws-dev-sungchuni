5064d783a243517a5419ff5b62c9acfcda460e96
sungchuni <new.sungchuni@gmail.com>
2020-01-19 13:29:00 +0900

    feat: add line page

--- src/Page/Line.js
+++ src/Page/Line.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { Line } from "ws-visu-obj";
+
 const Container = styled.div`
   canvas {
     display: block;
@@ -10,6 +12,52 @@ const Container = styled.div`
 
 export default class PageLine extends React.Component {
   render() {
-    return <Container></Container>;
+    return (
+      <Container>
+        <Line
+          data={[
+            { value: 1, title: "2019.5월", subtitle: "1,293,293원" },
+            { value: 2 + Math.random() ** 2, title: "", subtitle: "" },
+            { value: 3 + Math.random() ** 2, title: "", subtitle: "" },
+            { value: 4 + Math.random() ** 2, title: "", subtitle: "" },
+            { value: 5 + Math.random() ** 2, title: "", subtitle: "" },
+            { value: 6 + Math.random() ** 2, title: "", subtitle: "" },
+            { value: 7 + Math.random() ** 2, title: "", subtitle: "" },
+            { value: 8 + Math.random() ** 2, title: "", subtitle: "" },
+            { value: 9 + Math.random() ** 2, title: "", subtitle: "" },
+            { value: 10, title: "2020.1월", subtitle: "48,273,389원" }
+          ]}
+          options={{
+            title: "월별 상환 수익액 (세후)",
+            annotationTitleFontSize: 10,
+            annotationSubtitleFontSize: 12,
+            hasAxis: false
+          }}
+        />
+        <Line
+          data={[
+            [
+              { value: 1, title: "", subtitle: "" },
+              { value: 2 + Math.random() ** 2, title: "", subtitle: "" },
+              { value: 4 + Math.random() ** 2, title: "", subtitle: "" },
+              { value: 7 + Math.random() ** 2, title: "", subtitle: "" },
+              { value: 20, title: "aMAP", subtitle: "다중 대체자산" }
+            ],
+            [
+              { value: 1, title: "", subtitle: "" },
+              { value: 2 + Math.random() ** 2, title: "", subtitle: "" },
+              { value: 3 + Math.random() ** 2, title: "", subtitle: "" },
+              { value: 4 + Math.random() ** 2, title: "", subtitle: "" },
+              { value: 5 + Math.random(), title: "", subtitle: "단일 대체자산" }
+            ]
+          ]}
+          options={{
+            hasAxis: true,
+            annotationTitleFontSize: 12,
+            annotationSubtitleFontSize: 12
+          }}
+        />
+      </Container>
+    );
   }
 }
