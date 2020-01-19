4951b7b3e6318b80220d467e1a659602bcf7774e
sungchuni <new.sungchuni@gmail.com>
2020-01-20 02:58:42 +0900

    feat: add washing machine page

--- src/Page/WashingMachine.js
+++ src/Page/WashingMachine.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { WashingMachine } from "ws-visu-obj";
+
 const Container = styled.div`
   canvas {
     display: block;
@@ -10,6 +12,32 @@ const Container = styled.div`
 
 export default class PageWashingMachine extends React.Component {
   render() {
-    return <Container></Container>;
+    return (
+      <Container>
+        <WashingMachine
+          data={[
+            [
+              { key: 1, title: "3개월", link: [4, 5] },
+              { key: 2, title: "6개월", link: [1, 5, 6] },
+              { key: 3, title: "12개월", link: [1, 2, 3] }
+            ],
+            [
+              { key: 1, title: "주택담보ABL", link: [2] },
+              { key: 2, title: "기업매출채권", link: [2] },
+              { key: 3, title: "사행이익ABL", link: [2] },
+              { key: 4, title: "IoTFarm", link: [1] },
+              { key: 5, title: "CAPEX", link: [1, 3] },
+              { key: 6, title: "개인신용", link: [1, 3] }
+            ],
+            [
+              { key: 1, title: "7.6%" },
+              { key: 2, title: "10.9%" },
+              { key: 3, title: "8.3%" }
+            ]
+          ]}
+          options={{ width: 640, height: 320 }}
+        />
+      </Container>
+    );
   }
 }
